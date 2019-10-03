30dbdc6f88326b73f383a869415d258d37d670ad
sanjay mishra <jaysan.mishra@gmail.com>
2019-10-03 07:18:44 +0530

    Initial Commit

---
+++ StdInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StdInfo
+//
+//  Created by Logisch-apple01 on 03/10/19.
+//  Copyright © 2019 logischtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
